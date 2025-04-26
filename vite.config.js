da6c1c3293b528c0e01f0ad167bf7ed0e3cd5640
migue <mmoyano16@gmail.com>
2025-04-26 12:02:00 -0300

    upload docs for pages

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig, loadEnv } from 'vite';
+
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    base: '/gamedevjs-jam-2025/',
+  });
+}
